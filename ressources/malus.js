b1c28a7b86f77346158a699c6d8882e01ecb6dd3
simon <simon.tessard@gmail.com>
2023-12-05 10:37:43 +0100

    Reduced malus size

--- ressources/malus.js
+++ ressources/malus.js
@@ -1,5 +1,5 @@
 export default function Malus() {   
-	const geometry = new THREE.SphereGeometry(10, 32, 16 ); 
+	const geometry = new THREE.SphereGeometry(8, 32, 16 ); 
     const material = new THREE.MeshPhongMaterial( { color: 0xb20000 } ); 
     this.mesh = new THREE.Mesh( geometry, material );
     this.mesh.castShadow = true;
